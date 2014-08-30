0bbf46334aa2a285ed83e3e30df44b0eaf890e17
Ubuntu <ubuntu@ip-172-31-26-255.ap-southeast-2.compute.internal>
2014-08-30 09:15:10 +0000

    created output template

--- outputView.php
+++ outputView.php
@@ -8,13 +8,43 @@
     <head>
         <meta charset="UTF-8">
         <title>Wine Store Database Results Page</title>
+        <style>
+            table,th,td{border:1px solid black;}
+        </style>
     </head>
     <body>
         <a href = "index.php">Return to Form</a>
-        <?php if(count($query_result) != 0){ ?>
-        
-        <?php } else { ?>
+        <?php 
+            if(count($query_result) != 0){
+        ?>
+            <table>
+                <tr>
+                    <td>Wine</td>
+                    <td>Variety</td>
+                    <td>Year</td>
+                    <td>Winery</td>
+                    <td>Region</td>
+                    <td>Cost</td>
+                    <td> Available</td>
+                    <td>Stock Sold</td>
+                    <td>Sales Revenues</td>
+                </tr>
+        <?php
+                for($i = 0; $i < count($query_result); $i++){
+                    echo "<tr>\n";
+                    foreach ($query_result[$i] as $field => $value){
+                        echo "<td>" . $value . "</td>\n";
+                    }
+                    echo "</tr>\n";
+                }
+        ?>
+            </table>
+        <?php 
+            } else{ 
+        ?>
             There were no wines that fit your criteria
-        <?php } ?>
+        <?php 
+            } 
+        ?>
     </body>
 </html>
